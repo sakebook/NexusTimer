5490279645fcb18b2c2249eddfa8da012f179f45
Shinya Sakemoto <sakebook@gmail.com>
2016-06-02 01:35:40 +0900

    Add project

---
+++ app/src/main/java/com/sakebook/android/nexustimer/TvActivity.java
@@ -0,0 +1,13 @@
+package com.sakebook.android.nexustimer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TvActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tv);
+    }
+}
